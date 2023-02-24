cedb1bf76658202415c7b0beca3d50323d2556ac
Lettuce05 <romaine@unlv.nevada.edu>
2023-02-23 22:44:21 -0800

    Add Table component to display first sets

--- src/components/First/First.tsx
+++ src/components/First/First.tsx
@@ -1,20 +1,28 @@
 import { useContext } from 'preact/hooks'
 import {AppState} from '../../main'
 import Grammar from '../Grammar/Grammar';
+import Table from '../Table/Table';
 
 export default function First() {
   const {grammar} = useContext(AppState);
-
+  const tableHeaders = ['Non-Terminal', 'FIRST(X)']
+  let tableData = null;
   if (grammar.value !== null){
     // get first set
     grammar.value.getFirsts()
-    console.log(grammar.value.firsts)
+    tableData = Array.from(grammar.value.firsts).map(set => {
+      return [set[0], `{${Array.from(set[1]).sort().join(' , ')}}`]
+    })
+    console.log(tableData);
     // get follow set
     // get predict set
   }
   return (
     <div className="flex flex-1 max-h-[calc(100vh-56px)]">
       <Grammar />
+      <div className='flex-1'>
+        {grammar.value && tableData ? <Table headers={tableHeaders} rows={tableData} /> : null}
+      </div>
     </div>
   )
 }
